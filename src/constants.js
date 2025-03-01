991e5561f718520b7eedd1a5a4193ec0d207cf10
Prathamesh <pthombre657@gmail.com>
2025-03-02 01:32:55 +0530

    update backend url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const DEV_BASE_URL = "http://localhost:5000";
-export const PROD_BASE_URL = "https://globetrotter-quiz-api.onrender.com";
+export const PROD_BASE_URL = "https://globetrotter-backend-6e0r.onrender.com";
